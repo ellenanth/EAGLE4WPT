2b1fe3985d2932c2f62b43f8dc57aaefd585353c
ellenanth <anthonissen.ellen@gmail.com>
2017-02-21 14:44:54 +0100

    timer

--- src/main.c
+++ src/main.c
@@ -15,6 +15,49 @@
 
 int main(void)
 {
+	setupGPIO();
+	InitializeTimer();
 
-	for(;;);
+	for (;;)
+	{
+		int timerValue = TIM_GetCounter(TIM2);
+		if (timerValue == 400)
+			GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_SET);
+		else if (timerValue == 500)
+			GPIO_WriteBit(GPIOA, GPIO_Pin_5, Bit_RESET);
+	}
+}
+
+void setupGPIO()
+{
+	//GPIO AHB: enable clock van gpio voor led
+		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+		//pin 5 op input instellen (output is standaard) voor LED
+		GPIO_InitTypeDef gpioinit;
+		//opties: input, output, alternate function (AF), analog (AN)
+		gpioinit.GPIO_Mode = GPIO_Mode_OUT;
+		//opties: push pull, open drain -> altijd push pull
+		gpioinit.GPIO_OType = GPIO_OType_PP;
+		gpioinit.GPIO_Pin = GPIO_Pin_5;
+		//pull up - pull down
+		gpioinit.GPIO_PuPd = GPIO_PuPd_NOPULL;
+		gpioinit.GPIO_Speed = GPIO_Speed_Level_3;
+
+		//initialisatie afronden
+		GPIO_Init(GPIOA, &gpioinit);
+		GPIO_SetBits(GPIOA, GPIO_Pin_5);
+}
+
+void InitializeTimer()
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    TIM_TimeBaseInitTypeDef timerInitStructure;
+    timerInitStructure.TIM_Prescaler = 40000;
+    timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    timerInitStructure.TIM_Period = 500;
+    timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    timerInitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &timerInitStructure);
+    TIM_Cmd(TIM2, ENABLE);
 }
